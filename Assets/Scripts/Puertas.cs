6c4368d8717bb0f64fe9154dc2e693cdc3a16d69
David Villagrasa <107490517+David-Villagrasa@users.noreply.github.com>
2023-08-05 00:12:29 +0200

    Movimiento camara y tp bien

--- Assets/Scripts/Puertas.cs
+++ Assets/Scripts/Puertas.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class puertas : MonoBehaviour
 {
+    public GameObject Camera;
+    public Vector3 PosicionCamara;
     public GameObject Player;
     public Transform PosicionFutura;
     // Start is called before the first frame update
@@ -19,15 +21,21 @@ public class puertas : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("entre");
         if (collision.gameObject.CompareTag("Player"))
         {
+
             // Obtener el componente Transform del objeto Player
             Transform playerTransform = Player.GetComponent<Transform>();
 
             // Cambiar la posición del objeto Player a la posición de PosicionFutura
             playerTransform.position = PosicionFutura.position;
+
+            Transform cameraTransform = Camera.GetComponent<Transform>();
+            cameraTransform.position = PosicionCamara;
         }
     }
+
 }
